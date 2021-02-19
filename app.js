826e9808d57471ab3a14f355702ab57017fdbeb1
Brandon Lau <brandonlau@Brandons-MacBook-Pro.local>
2021-02-19 14:48:48 -0500

    make utils and path.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
@@ -12,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>404 ERROR PAGE NOT FOUND </h1>`);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,8 @@ const router = express.Router();
 
 const path = require("path");
 
-router.get("/hello", (req, res, next) => {
-  res.send("<h1>Hello!! </h1>");
-});
-
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
